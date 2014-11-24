289c67c0261727eb80701f05a855c58a27811298
positive-attitude <aviationproject101@gmail.com>
2014-11-24 12:36:59 -0800

    Admin: edit and add forms
    Here are two admin forms.

---
+++ admin_main.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/awesomestyle.css">
+
+    <title>Aviation: Edit content</title>
+
+</head>
+
+<body>
+
+<ul>
+<li><a href="admin_add.php">Add new flashcard</a></li>
+</ul>
+
+
+<h4>Edit flashcards</h4>
+
+
+<ul>
+<li><a href="">Definition</a></li>    
+</ul>
+	
+</body>
+</html>
