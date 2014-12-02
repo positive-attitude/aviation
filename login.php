474f5ac403e1589b33ac1ea0c833280d7ca43f6f
Jim Tryon <jtryon@mail.greenriver.edu>
2014-12-01 17:00:01 -0800

    Updated login pages

--- login.php
+++ login.php
@@ -4,26 +4,4 @@ $file = "passwords.txt";
 $username = htmlspecialchars($_POST['username']);
 $password = md5($_POST['password']);
 
-$file = "passwords.txt";
-
-if (!file) 
-{
-	echo "Unable to open file";
-}
-
-if (is_array($userpass)) {
-	
-	foreach ($login as $userpass)
-{
-	// Split the strings one by one
-		list($user, $pass) = explode(":", $userpass);
-
-		if (($username == $user) AND ($password == $pass))
-		{
-			// Login successful
-			
-	
-		}
-	}
-}
 ?>
